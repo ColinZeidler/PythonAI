e3f21da811179fe56a5aa53c44c2937943d26694
Colin Zeidler <c.zeidler.wc@gmail.com>
2017-03-10 20:24:08 -0500

    human players can play, but not quite done move validation

--- A2/Game.py
+++ A2/Game.py
@@ -4,9 +4,75 @@ from A2.Player import Human, Computer
 
 class Game(object):
     def __init__(self):
-        self.p1 = Human(self, 1)
-        self.p2 = Computer(self, 2)
+        self.p1 = Human(self, "R")
+        self.p2 = Computer(self, "G")
         self.board = GameBoard()
+        self.board.setup_2player(self.p1, self.p2)
+
+    def play(self):
+        keep_playing = True
+        while keep_playing:
+            # check if p1 owns any towers
+            print("Player", self.p1.id, "turn")
+            move = self.p1.get_move()
+            self.board.apply_move(move)
+
+            # check if p2 owns any towers
+            # print("Player", self.p1.id, "turn")
+            # move = self.p2.get_move()
+            # self.board.apply_move(move)
+
+            if False:
+                keep_playing = False
+
+    def ok_pos(self, x, y):
+
+        # Check that pos is within bounds of board
+        if x > 7:
+            return False
+        if y > 7:
+            return False
+        if x < 0:
+            return False
+        if y < 0:
+            return False
+
+        # check that pos is not in the deleted corners
+        if self.board[y][x] is None:
+            return False
+
+        return True
+
+    def valid_move(self, move, player_id):
+        """:returns: True if the move is a valid move, False otherwise
+        Things to check:
+        valid start and end positions
+        Move distance is <= to number of pieces moving
+        Number of pieces moving is <= to number of pieces at starting pos
+        Player making move owns the piece at top of the stack"""
+        start = move[0]
+        dest = move[1]
+        if start == dest:
+            return False
+
+        xdiff = abs(start[0] - dest[0])
+        ydiff = abs(start[1] - dest[1])
+        # TODO only allowed to move in one axis, total move must be less or equal than the number moving
+        if False:
+            return False
+
+        if not self.ok_pos(start[0], start[1]):
+            return False
+        if not self.ok_pos(dest[0], dest[1]):
+            return False
+
+        if start[2] > len(self.board[start[1]][start[0]]):
+            return False
+
+        if self.board[start[1]][start[0]][-1] != player_id:
+            return False
+
+        return True
 
 
 class GameBoard(object):
@@ -21,15 +87,82 @@ class GameBoard(object):
                            [None, None, [], [], [], [], None, None]
                            ]
 
-    def setup_2player(self):
-        pass
+    def setup_2player(self, p1, p2):
+        self.boardState[1][1].append(p1.id)
+        self.boardState[1][2].append(p1.id)
+        self.boardState[1][3].append(p2.id)
+        self.boardState[1][4].append(p2.id)
+        self.boardState[1][5].append(p1.id)
+        self.boardState[1][6].append(p1.id)
+
+        self.boardState[2][1].append(p2.id)
+        self.boardState[2][2].append(p2.id)
+        self.boardState[2][3].append(p1.id)
+        self.boardState[2][4].append(p1.id)
+        self.boardState[2][5].append(p2.id)
+        self.boardState[2][6].append(p2.id)
+
+        self.boardState[3][1].append(p1.id)
+        self.boardState[3][2].append(p1.id)
+        self.boardState[3][3].append(p2.id)
+        self.boardState[3][4].append(p2.id)
+        self.boardState[3][5].append(p1.id)
+        self.boardState[3][6].append(p1.id)
+
+        self.boardState[4][1].append(p2.id)
+        self.boardState[4][2].append(p2.id)
+        self.boardState[4][3].append(p1.id)
+        self.boardState[4][4].append(p1.id)
+        self.boardState[4][5].append(p2.id)
+        self.boardState[4][6].append(p2.id)
+
+        self.boardState[5][1].append(p1.id)
+        self.boardState[5][2].append(p1.id)
+        self.boardState[5][3].append(p2.id)
+        self.boardState[5][4].append(p2.id)
+        self.boardState[5][5].append(p1.id)
+        self.boardState[5][6].append(p1.id)
+
+        self.boardState[6][1].append(p2.id)
+        self.boardState[6][2].append(p2.id)
+        self.boardState[6][3].append(p1.id)
+        self.boardState[6][4].append(p1.id)
+        self.boardState[6][5].append(p2.id)
+        self.boardState[6][6].append(p2.id)
 
     def setup_4player(self):
         pass
 
+    def apply_move(self, move):
+        sx = move[0][0]
+        sy = move[0][1]
+        dx = move[1][0]
+        dy = move[1][1]
+        c = move[0][2]
+        tmp = self.boardState[sy][sx][-c:]
+        del self.boardState[sy][sx][-c:]
+
+        self.boardState[dy][dx].extend(tmp)
+
+    def __getitem__(self, item):
+        return self.boardState[item]
+
+    def __len__(self):
+        return len(self.boardState)
+
     def __str__(self):
-        return repr(self.boardState)
+        my_string = "     0   1   2   3   4   5   6   7\n"
+        for y, row in enumerate(self.boardState):
+            my_string += str(y) + " [ "
+            for x, item in enumerate(row):
+                if item is None:
+                    my_string += "XXX "
+                else:
+                    my_string += str(len(item)) + ":" + str(item[-1] if len(item) > 0 else "X") + " "
+            my_string += "]\n"
+        return my_string
 
 if __name__ == "__main__":
     print("Starting 2 player game")
     game = Game()
+    game.play()
--- A2/Player.py
+++ A2/Player.py
@@ -18,10 +18,64 @@ class Player(object):
 class Human(Player):
     def get_move(self):
         """Prompts the player for input"""
-        pass
+        # let the player inspect the board to plan
+        planning = True
+        while planning:
+            print("To view the board type B")
+            print("To view a stack type S")
+            print("To enter your move type M")
+            choice = input("> ").upper()
+            if choice == "B":
+                print(self.game.board)
+            elif choice == "S":
+                x, y = get_pos(self.game.ok_pos)
+                print("Stack looks like:")
+                print("Bottom", self.game.board[y][x], "Top")
+            elif choice == "M":
+                planning = False
+            else:
+                print("Invalid Choice")
+
+        # Create move
+        planning = True
+        while planning:
+            # get start location
+            print("Start location:")
+            start_x, start_y = get_pos(self.game.ok_pos)
+            print("Destination")
+            dest_x, dest_y = get_pos(self.game.ok_pos)
+            count = get_int("Number of pieces to move")
+            move = ((start_x, start_y, count), (dest_x, dest_y, count))
+            planning = not self.game.valid_move(move, self.id)
+            if planning:
+                print("Invalid move")
+        return move
+
+
+def get_pos(check_func):
+    bad_pos = True
+    while bad_pos:
+        x = get_int("Column / X")
+        y = get_int("Row / Y")
+        bad_pos = not check_func(x, y)
+        if bad_pos:
+            print("Invalid location")
+    return x, y
+
+
+def get_int(text):
+    bad = True
+    while bad:
+        print(text)
+        try:
+            x = int(input("> "))
+            bad = False
+        except ValueError:
+            print("Enter an integer")
+    return x
 
 
 class Computer(Player):
     def get_move(self):
-        """Looks at the current board state, and calculates possible moves"""
+        """Looks at the current board state, and calculates best move"""
         pass
