d4e344cf73eb1ad4da5d27fef55fa597b669c429
Colin Zeidler <c.zeidler.wc@gmail.com>
2017-04-02 21:43:17 -0400

    nearly done dependency tree?

--- A3/Training.py
+++ A3/Training.py
@@ -1,13 +1,115 @@
 from A3.Data import get_random_data, get_real_data
 import operator
 import functools
+import itertools
+import math
+
+
+class Vertex(object):
+    """This is a feature"""
+
+    def __init__(self, feature_id):
+        self.fid = feature_id
+        self.p0 = None
+        self.p1 = None
+
+    def calc_p(self, data_set):
+        my_sum = 0
+        for d in data_set:
+            my_sum = my_sum + 1 if d[self.fid] == 0 else my_sum
+        self.p0 = my_sum / len(data_set)
+        self.p1 = 1 - self.p0
+
+    def __hash__(self):
+        return self.fid
+
+    def __str__(self):
+        return str(self.fid)
+
+
+class Edge(object):
+    def __init__(self, v1, v2):
+        self.v1 = v1
+        self.v2 = v2
+        self.p00 = None
+        self.p11 = None
+        self.p10 = None
+        self.p01 = None
+        self.weight = None
+
+    def calc_ps(self, data_set):
+        s00 = 0
+        s11 = 0
+        s10 = 0
+        s01 = 0
+        for d in data_set:
+            if d[self.v1.fid] == 1:
+                if d[self.v2.fid] == 1:
+                    s11 += 1
+                else:
+                    s10 += 1
+            else:
+                if d[self.v2.fid] == 1:
+                    s01 += 1
+                else:
+                    s00 += 1
+        self.p00 = s00 / len(data_set)
+        self.p11 = s11 / len(data_set)
+        self.p01 = s01 / len(data_set)
+        self.p10 = s10 / len(data_set)
+
+    def __str__(self):
+        return str(self.v1) + "-" + str(self.weight) + "->" + str(self.v2)
 
 
 # estimate unknown dependence tree
-# 1. calculate individual probability for each feature in the current class
 # 2. check probabilities of a feature when another feature is 1 or 0
-def dependece_tree_estimator(test_data):
-    pass
+def dependence_tree_estimator(test_data):
+
+    # combine data from each class into single list
+    combined_data = []
+    for c in test_data:
+        combined_data.extend(c)
+
+    print(len(combined_data))
+    # calculate indiviual probabilities for each feature
+    vertexs = [Vertex(i) for i in range(len(combined_data[0]))]
+    for v in vertexs:
+        v.calc_p(combined_data)
+    edges = itertools.combinations(vertexs, 2)
+    edges = [Edge(x[0], x[1]) for x in edges]
+    for edge in edges:
+        # calculate probabilities for feature pairs to be each value
+        edge.calc_ps(combined_data)
+        # use EMIM function to calculate weights,
+        # i=0, j=0
+        weight = edge.p00*math.log2(edge.p00/(edge.v1.p0 * edge.v2.p0))
+        # i=1, j=0
+        weight += edge.p00*math.log2(edge.p10/(edge.v1.p1 * edge.v2.p0))
+        # i=0, j=1
+        weight += edge.p00*math.log2(edge.p01/(edge.v1.p0 * edge.v2.p1))
+        # i=1, j=1
+        weight += edge.p11*math.log2(edge.p11/(edge.v1.p1 * edge.v2.p1))
+
+        # print(weight)
+        edge.weight = weight
+
+    edges = sorted(edges, key=lambda x: x.weight, reverse=True)
+
+    # tree time!
+    graph_vs = set()
+    graph_es = []
+    for edge in edges:
+        if edge.v1 in graph_vs and edge.v2 in graph_vs:
+            continue
+        graph_vs.add(edge.v1)
+        graph_vs.add(edge.v2)
+
+        graph_es.append(edge)
+
+    for e in graph_es:
+        print(e)
+
 
 
 def bayesian_independent_trainer(training_data):
@@ -89,5 +191,6 @@ def five_fold_validation(input_data_set, trainer=bayesian_independent_trainer,
 
 if __name__ == "__main__":
     d = get_random_data()
-    accuracy = five_fold_validation(d)
-    print(accuracy*100)
+    dependence_tree_estimator(d)
+    # accuracy = five_fold_validation(d)
+    # print(accuracy*100)
